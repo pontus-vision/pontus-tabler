444f99823404ee079b5961312a32ba033ad31a1c
Paulo <pvpmartins@hotmail.com>
2023-04-26 19:23:45 -0300

    aggrid onfirstdatarendered type added

---
+++ src/types.ts
@@ -0,0 +1,100 @@
+import { ColumnApi, GridApi } from "ag-grid-community";
+
+export interface AgGrigFirstDataRenderedEvent<TData = any, TContext = any> {
+  // Index of the first rendered row
+  firstRow: number;
+  // Index of the last rendered row
+  lastRow: number;
+  // The grid api.
+  api: GridApi<TData>;
+  // The column api.
+  columnApi: ColumnApi;
+  // Application context as set on `gridOptions.context`.
+  context: TContext;
+  // Event identifier
+  type: string;
+}
+
+export interface ModelContentList {
+  data: ModelContentListData[];
+  meta: Meta;
+}
+
+export interface ModelContentListData {
+  id: string;
+  entryId: string;
+  createdOn: Date;
+  savedOn: Date;
+  createdBy: string;
+  ownedBy: string;
+  subsistemaReferencia: string;
+}
+
+export interface Meta {
+  cursor: null;
+  totalCount: number;
+}
+
+export interface ModelColName {
+  id: string;
+  fieldId: string;
+  storageId: string;
+  type: string;
+  label: string;
+  placeholderText: string;
+  helpText: null;
+  predefinedValues: null;
+  multipleValues: null;
+  renderer: Renderer;
+  validation: Validation[];
+  listValidation: null;
+  settings: null;
+  __typename: string;
+}
+
+export interface Renderer {
+  name: string;
+  __typename: string;
+}
+
+export interface Validation {
+  name: string;
+  settings: null;
+  message: string;
+  __typename: string;
+}
+
+export interface CmsEntriesList {
+  content: CmsEntriesListContent;
+}
+
+export interface CmsEntriesListContent {
+  data: CmsEntriesListContentData[];
+  meta: CmsEntriesListContentMeta;
+  error: null;
+  __typename: string;
+}
+
+export interface CmsEntriesListContentData {
+  id: string;
+  savedOn: Date;
+  meta: CmsEntriesListContentDataMeta;
+  basesLegaisReferencia: string;
+  __typename: string;
+}
+
+export interface CmsEntriesListContentDataMeta {
+  title: string;
+  publishedOn: Date;
+  version: number;
+  locked: boolean;
+  status: string;
+  __typename: string;
+}
+
+export interface CmsEntriesListContentMeta {
+  cursor: null;
+  hasMoreItems: boolean;
+  totalCount: number;
+  __typename: string;
+}
