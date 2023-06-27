e1ad3869b5ca53498e318eb873b157d5ade02e9e
Paulo <pvpmartins@hotmail.com>
2023-06-26 23:06:24 -0300

    wip

--- src/i18n.js
+++ src/i18n.js
@@ -6,7 +6,7 @@ import enTranslation from './translations/en.json'
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const resources = {
+export const resources = {
   en: {
     translation: enTranslation
   },
