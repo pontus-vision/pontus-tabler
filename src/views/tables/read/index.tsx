2b41d3125ef5ef65914202af3eed89f73f28f53a
Paulo <pvpmartins@hotmail.com>
2023-08-11 14:58:03 -0300

    wip

--- src/views/tables/read/index.tsx
+++
@@ -1,8 +0,0 @@
-import PVFlexLayout from '../../../pv-react/PVFlexLayout';
-import PVGridWebiny2 from '../../../pv-react/PVGridWebiny2';
-
-const TablesReadView = () => {
-  return <PVGridWebiny2 />;
-};
-
-export default TablesReadView;
