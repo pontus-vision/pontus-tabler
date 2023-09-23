a42e25355b4973c9468c4ab7cd96138ef5dfab27
pipeline role <codepipeline@hmpotr.com>
2023-09-23 16:58:07 +0100

    pvhome-#22: added sample cosmosdb code - added env vars for endpoint and key

--- backend/cosmos-db/docker/test/app.js
+++ backend/cosmos-db/docker/test/app.js
@@ -2,8 +2,8 @@ import { CosmosClient } from '@azure/cosmos'
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
 const cosmosClient = new CosmosClient({
-  endpoint: 'https://localhost:8081/',
-  key: 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
+  endpoint: process.env.PH_COSMOS_ENDPOINT || 'https://localhost:8081/',
+  key: process.env.PH_COSMOS_KEY ||'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
 })
 const { database } = await cosmosClient.databases.createIfNotExists({
   id: 'cosmicworks',
