5d23d5510cedcac51be4c83d1456bdcb6da64b23
Paulo <pvpmartins@hotmail.com>
2023-10-11 23:02:20 -0300

    testing Cosmosdb

--- backend/cosmos-db/docker/test/app.js
+++ backend/cosmos-db/docker/test/app.js
@@ -1,47 +1,45 @@
-import { CosmosClient } from '@azure/cosmos';
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+import { CosmosClient } from '@azure/cosmos'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
 const cosmosClient = new CosmosClient({
   endpoint: process.env.PH_COSMOS_ENDPOINT || 'https://localhost:8081/',
-  key:
-    process.env.PH_COSMOS_KEY ||
-    'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==',
-});
+  key: process.env.PH_COSMOS_KEY ||'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
+})
 const { database } = await cosmosClient.databases.createIfNotExists({
   id: 'cosmicworks',
-  throughput: 400,
-});
+  throughput: 400
+})
 
-const { container: tablesContainer } =
-  await database.containers.createIfNotExists({
-    id: 'products',
-    partitionKey: {
-      paths: ['/id'],
-    },
-  });
+const { container } = await database.containers.createIfNotExists({
+  id: 'products',
+  partitionKey: {
+    paths: [
+      '/id'
+    ]
+  }
+})
 
 const item = {
   id: '68719518371',
-  name: 'Kiama classic surfboard',
-};
+  name: 'Kiama classic surfboard'
+}
 
-export const createTable = () => {
-  return tablesContainer.items.create(item);
-};
+container.items.upsert(item)
+
 
 const querySpec = {
-  query: 'select * from products p where p.name=@name',
-  parameters: [
-    {
-      name: '@name',
-      value: 'Kiama classic surfboard',
-    },
-  ],
+    query: "select * from products p where p.name=@name",
+    parameters: [
+        {
+            name: "@name",
+            value: 'Kiama classic surfboard'
+        }
+    ]
 };
 
-// Get items
-const { resources } = await tablesContainer.items.query(querySpec).fetchAll();
+// Get items 
+const { resources } = await container.items.query(querySpec).fetchAll();
 
 for (const item of resources) {
-  console.log(`${item.id}: ${item.name}, ${item.sku}`);
+    console.log(`${item.id}: ${item.name}, ${item.sku}`);
 }
