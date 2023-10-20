efb49b98f1393f642b41f00f2fe6648b8019330a
Paulo <pvpmartins@hotmail.com>
2023-10-20 17:27:41 -0300

    new handle function to fetch database

--- backend/cosmos-db/src/utils/cosmos-utils.ts
+++ backend/cosmos-db/src/utils/cosmos-utils.ts
@@ -1,6 +1,5 @@
-import { CosmosClient } from '@azure/cosmos';
+import { CosmosClient, Database, DatabaseResponse } from '@azure/cosmos';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const cosmosClient = new CosmosClient({
   endpoint: process.env.PH_COSMOS_ENDPOINT || 'https://localhost:8081/',
   key:
@@ -8,7 +7,9 @@ const cosmosClient = new CosmosClient({
     'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==',
 });
 
-export const fetchDatabase = async (db_name: string) => {
+export const fetchDatabase = async (
+  db_name: string,
+): Promise<Database | undefined> => {
   try {
     const { database } = await cosmosClient.databases.createIfNotExists({
       id: db_name,
