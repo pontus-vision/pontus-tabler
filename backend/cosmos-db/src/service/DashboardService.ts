73a81f07341cf95400b0da1555dd46b70e94ecd7
Paulo <pvpmartins@hotmail.com>
2023-10-25 18:11:18 -0300

    new func to return Dashboards count

--- backend/cosmos-db/src/service/DashboardService.ts
+++ backend/cosmos-db/src/service/DashboardService.ts
@@ -5,7 +5,11 @@ import {
   ReadPaginationFilter,
 } from 'pontus-tabler/src/pontus-api/typescript-fetch-client-generated';
 import { DataRoot } from 'pontus-tabler/src/types';
-import { fetchDashboardsContainer, fetchDatabase } from '../utils/cosmos-utils';
+import {
+  fetchDashboardsContainer,
+  fetchDatabase,
+  filterToQuery,
+} from '../utils/cosmos-utils';
 import { Container } from '@azure/cosmos';
 
 export const upsertDashboard = async (
@@ -65,102 +69,16 @@ export const deleteDashboard = async (data: DashboardDeleteReq) => {
 
 export const readDashboards = async (body: ReadPaginationFilter) => {
   // let query = 'select * from dashboards d';
-  const query = [];
 
-  const cols = body?.filters;
-
-  for (const colId in cols) {
-    console.log(colId);
-    if (cols.hasOwnProperty(colId)) {
-      const condition1Filter = cols[colId]?.condition1?.filter;
-      const condition2Filter = cols[colId]?.condition2?.filter;
-
-      const type1 = cols[colId]?.condition1?.type.toLowerCase();
-      const type2 = cols[colId]?.condition2?.type.toLowerCase();
-
-      const operator = cols[colId]?.operator;
-
-      if (condition1Filter && type1 === 'contains') {
-        query.push(` WHERE CONTAINS(d.${colId}, "${condition1Filter}")`);
-      }
-
-      if (condition2Filter && type2 === 'contains') {
-        query.push(` ${operator} CONTAINS(d.${colId}, "${condition2Filter}")`);
-      }
-
-      if (condition1Filter && type1 === 'not contains') {
-        query.push(` WHERE NOT CONTAINS(d.${colId}, "${condition1Filter}")`);
-      }
-
-      if (condition2Filter && type2 === 'not contains') {
-        query.push(
-          ` ${operator} NOT CONTAINS(d.${colId}, "${condition2Filter}")`,
-        );
-      }
-
-      if (condition1Filter && type1 === 'starts with') {
-        query.push(` WHERE STARTSWITH(d.${colId}, "${condition1Filter}")`);
-      }
-
-      if (condition2Filter && type2 === 'starts with') {
-        query.push(
-          ` ${operator} STARTSWITH(d.${colId}, "${condition2Filter}")`,
-        );
-      }
-
-      if (condition1Filter && type1 === 'ends with') {
-        query.push(` WHERE ENDSWITH(d.${colId}, "${condition1Filter}")`);
-      }
-
-      if (condition2Filter && type2 === 'ends with') {
-        query.push(` ${operator} ENDSWITH(d.${colId}, "${condition2Filter}")`);
-      }
-
-      if (condition1Filter && type1 === 'equals') {
-        query.push(` WHERE d.${colId} = "${condition1Filter}"`);
-      }
-
-      if (condition2Filter && type2 === 'equals') {
-        query.push(` ${operator} d.${colId} = "${condition2Filter}"`);
-      }
-
-      if (condition1Filter && type1 === 'not equals') {
-        query.push(` WHERE NOT d.${colId} = "${condition1Filter}"`);
-      }
-
-      if (condition2Filter && type2 === 'not equals') {
-        query.push(` ${operator} NOT d.${colId} = "${condition2Filter}"`);
-      }
-    }
-  }
-
-  for (let i = 0; i < query.length; i++) {
-    // Replace the first occurrence of "WHERE" with "AND" in each element
-    if (i > 0) {
-      query[i] = query[i].replace('WHERE', 'AND');
-    }
-  }
+  const query = filterToQuery(body);
 
   const querySpec = {
-    query: 'select * from dashboards d ' + query.join(''),
-    parameters: [
-      {
-        name: '@colId',
-        value: Object.keys(body?.filters)[0],
-      },
-      {
-        name: '@offset',
-        value: body.from - 1,
-      },
-      {
-        name: '@limit',
-        value: body.to - body.from + 1,
-      },
-    ],
+    query,
+    parameters: [],
   };
 
   const dashboardContainer = await fetchDashboardsContainer();
-  console.log({ dashboardContainer });
+  console.log({ query: querySpec.query });
   const { resources } = await dashboardContainer.items
     .query(querySpec)
     .fetchAll();
@@ -171,4 +89,13 @@ export const readDashboards = async (body: ReadPaginationFilter) => {
   }
 
   return resources;
+};
+
+export const countDashboardsRecords = async (): Promise<number> => {
+  const dashboardContainer = await fetchDashboardsContainer();
+  const { resources } = await dashboardContainer.items
+    .query({ query: 'SELECT VALUE COUNT(1) FROM dashboards', parameters: [] })
+    .fetchAll();
+
+  return resources[0];
 };
