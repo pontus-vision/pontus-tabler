fd68c0cf2ff7069f4ec8f7453781729fa0972661
Paulo <pvpmartins@hotmail.com>
2023-11-08 16:45:45 -0300

    added handle funcs for crud

--- frontend/src/views/dashboards/Dashboards.tsx
+++ frontend/src/views/dashboards/Dashboards.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
 import PVGridWebiny2 from '../../pv-react/PVGridWebiny2';
 import {
+  BaseModelRef,
+  DashboardRef,
   DashboardsReadReq,
   DashboardsReadRes,
   ReadPaginationFilter,
   ReadPaginationFilterFilters,
 } from '../../pontus-api/typescript-fetch-client-generated';
 import { ColDef, RowEvent } from 'ag-grid-community';
-import { getAllDashboards } from '../../client';
+import { deleteDashboard, getAllDashboards } from '../../client';
 import { capitalizeFirstLetter } from '../../webinyApi';
 import { useDispatch } from 'react-redux';
 import { setDashboardId } from '../../store/sliceDashboards';
@@ -16,9 +18,7 @@ import PVFlexLayout from '../../pv-react/PVFlexLayout';
 import DashboardView from '../DashboardView';
 import { isEmpty } from '../../helpers/functions';
 
-interface Row extends RowEvent<any, any> {
-  id: string;
-}
+interface DashboardRefId extends DashboardRef, BaseModelRef {}
 
 const Dashboards = () => {
   const dispatch = useDispatch();
@@ -53,61 +53,58 @@ const Dashboards = () => {
     [key: string]: ReadPaginationFilterFilters;
   }>({});
   const [from, setFrom] = useState<number>(1);
-  const [to, setTo] = useState<number>(5);
+  const [to, setTo] = useState<number>(8);
   const [totalCount, setTotalCount] = useState<number>(2);
   const [rowClicked, setRowClicked] = useState<{ [key: string]: any }>();
-  const [newDashboardName, setNewDashboardName] = useState<string>();
+
   const [deletion, setDeletion] = useState(false);
   const navigate = useNavigate();
 
-  const [newDashboard, setNewDashboard] = useState(false);
+  const fetchDashboars = async () => {
+    console.log('fetching');
+    try {
+      const req: ReadPaginationFilter = {
+        from,
+        to,
+        filters,
+      };
+      const res = await getAllDashboards(req);
+      console.log({ res, req });
 
-  useEffect(() => {
-    const fetchDashboars = async () => {
-      console.log('fetching');
-      try {
-        const req: ReadPaginationFilter = {
-          from,
-          to,
-          filters,
+      const rowsVal = res?.data?.dashboards?.map((dashboard) => {
+        return {
+          owner: dashboard?.owner,
+          folder: dashboard?.folder,
+          name: dashboard?.name,
+          id: dashboard?.id,
         };
-        const res = await getAllDashboards(req);
-        console.log({ res, req });
+      });
 
-        const rowsVal = res?.data?.dashboards?.map((dashboard) => {
-          return {
-            owner: dashboard?.owner,
-            folder: dashboard?.folder,
-            name: dashboard?.name,
-            id: dashboard?.id,
+      const totalRows = res?.data?.totalDashboards;
+
+      const obj: ColDef[] | undefined = rowsVal
+        ?.map((dashboard) => {
+          return Object.keys(dashboard);
+        })[0]
+        .map((header) => {
+          const obj: ColDef = {
+            headerName: capitalizeFirstLetter(header),
+            field: header,
+            filter: true,
+            sortable: true,
           };
+          return obj;
         });
+      console.log({ rowsVal, obj, totalRows });
 
-        const totalRows = res?.data?.totalDashboards;
-
-        const obj: ColDef[] | undefined = rowsVal
-          ?.map((dashboard) => {
-            return Object.keys(dashboard);
-          })[0]
-          .map((header) => {
-            const obj: ColDef = {
-              headerName: capitalizeFirstLetter(header),
-              field: header,
-              filter: true,
-              sortable: true,
-            };
-            return obj;
-          });
-        console.log({ rowsVal, obj, totalRows });
-
-        setRows(rowsVal);
-        setTotalCount(totalRows || 0);
-        obj && setCols(obj);
-      } catch (error) {
-        setRows([]);
-      }
-    };
-
+      setRows(rowsVal);
+      setTotalCount(totalRows || 0);
+      obj && setCols(obj);
+    } catch (error) {
+      setRows([]);
+    }
+  };
+  useEffect(() => {
     fetchDashboars();
   }, [filters, to, from]);
 
@@ -120,7 +117,7 @@ const Dashboards = () => {
     console.log({ rowClicked });
     dispatch(setDashboardId({ id: rowClicked.id }));
 
-    navigate(`/dashboard/${rowClicked.id}`);
+    navigate(`/dashboard/update/${rowClicked.id}`);
   };
 
   const handleAddition = () => {
@@ -130,7 +127,6 @@ const Dashboards = () => {
   useEffect(() => {
     setDashboard();
   }, [rowClicked]);
-  // if (!rows) return;
 
   const handleFiltersChange = (filters: {
     [key: string]: ReadPaginationFilterFilters;
@@ -140,41 +136,50 @@ const Dashboards = () => {
     setFilters(filters);
   };
 
+  const handleRefresh = () => {
+    fetchDashboars();
+  };
+
+  const handleDelete = (arr: DashboardRefId[]) => {
+    arr.forEach(async (item) => {
+      const res = await deleteDashboard(item.id);
+      console.log(res);
+    });
+    handleRefresh();
+  };
+
+  if (!rows) return;
+
   return (
     <div className="top-12 relative">
-      {!newDashboard && (
-        <>
-          <PVGridWebiny2
-            totalCount={totalCount}
-            // setDeletion={set}
-            rows={rows}
-            add={handleAddition}
-            cols={cols}
-            onFiltersChange={handleFiltersChange}
-            // setFrom={setFrom}
-            // setTo={setTo}
-            setRowClicked={setRowClicked}
-          />
+      <>
+        <PVGridWebiny2
+          totalCount={totalCount}
+          setDeletion={setDeletion}
+          rows={rows}
+          add={handleAddition}
+          cols={cols}
+          onDelete={handleDelete}
+          onFiltersChange={handleFiltersChange}
+          // setFrom={setFrom}
+          // setTo={setTo}
+          onRefresh={handleRefresh}
+          setRowClicked={setRowClicked}
+          permissions={{
+            updateAction: true,
+            createAction: true,
+            deleteAction: true,
+            readAction: true,
+          }}
+        />
 
-          <button
-            onClick={() => setNewDashboard(true)}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded shadow-sm hover:shadow-md"
-          >
-            Add New Dashboard
-          </button>
-        </>
-      )}
-
-      {newDashboard && (
-        <>
-          <label htmlFor="">Dashboard Name: </label>
-          <input
-            type="text"
-            onChange={(e) => setNewDashboardName(e.target.value)}
-          />
-          <DashboardView createMode={true} dashboardName={newDashboardName} />
-        </>
-      )}
+        <button
+          onClick={() => handleAddition()}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded shadow-sm hover:shadow-md"
+        >
+          Add New Dashboard
+        </button>
+      </>
     </div>
   );
 };
