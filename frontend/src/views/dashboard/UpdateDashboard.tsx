5baa7193ee320c79e2515618bb8aebf6ce0bf28d
Paulo <pvpmartins@hotmail.com>
2023-11-09 11:33:36 -0300

    implemented input for name

--- frontend/src/views/dashboard/UpdateDashboard.tsx
+++ frontend/src/views/dashboard/UpdateDashboard.tsx
@@ -5,20 +5,31 @@ import {
   DashboardUpdateReq,
 } from '../../pontus-api/typescript-fetch-client-generated';
 import DashboardView from '../DashboardView';
+import { useState } from 'react';
 
 const UpdateDashboard = () => {
   const { id } = useParams();
+  const [newDashboardName, setNewDashboardName] = useState<string>();
 
   const saveDashboard = async (obj: DashboardRef) => {
     if (!id) return;
     try {
-      const res = await updateDashboard({ ...obj, id });
+      const res = await updateDashboard({
+        ...obj,
+        id,
+        name: newDashboardName || obj.name,
+      });
 
       console.log({ res });
     } catch (error) {}
   };
   return (
     <>
+      <label htmlFor="">Dashboard Name: </label>
+      <input
+        type="text"
+        onChange={(e) => setNewDashboardName(e.target.value)}
+      />
       <DashboardView onDashboardSave={saveDashboard} />
     </>
   );
