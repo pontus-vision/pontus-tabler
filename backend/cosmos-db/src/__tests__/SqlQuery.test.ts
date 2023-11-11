dfdba2a9c1689b7207b84e4b377cfdf2794cbd8d
Paulo <pvpmartins@hotmail.com>
2023-11-10 21:31:52 -0300

    expected result changed

--- backend/cosmos-db/src/__tests__/SqlQuery.test.ts
+++ backend/cosmos-db/src/__tests__/SqlQuery.test.ts
@@ -21,7 +21,7 @@ describe('Filter to SQL', () => {
     const query = filterToQuery(readBody2);
 
     expect(query.toLocaleLowerCase()).toBe(
-      'select * from dashboards d where contains(d.name, "pontusvision") and contains(d.folder, "folder 1")',
+      'where contains(d.name, "pontusvision") and contains(d.folder, "folder 1")',
     );
 
     const query2 = filterToQuery({
@@ -40,7 +40,7 @@ describe('Filter to SQL', () => {
     });
 
     expect(query2.toLocaleLowerCase()).toBe(
-      'select * from dashboards d where d.name = "pontusvision" and d.folder = "folder 1"',
+      'where d.name = "pontusvision" and d.folder = "folder 1"',
     );
 
     const query3 = filterToQuery({
@@ -59,7 +59,7 @@ describe('Filter to SQL', () => {
     });
 
     expect(query3.toLocaleLowerCase()).toBe(
-      'select * from dashboards d where not contains(d.name, "pontusvision") and not contains(d.folder, "folder 1")',
+      'where not contains(d.name, "pontusvision") and not contains(d.folder, "folder 1")',
     );
 
     const query4 = filterToQuery({
@@ -78,7 +78,7 @@ describe('Filter to SQL', () => {
     });
 
     expect(query4.toLocaleLowerCase()).toBe(
-      'select * from dashboards d where not d.name = "pontusvision" and not d.folder = "folder 1"',
+      'where not d.name = "pontusvision" and not d.folder = "folder 1"',
     );
 
     const date = '2023-10-19 00:00:00';
@@ -117,7 +117,7 @@ describe('Filter to SQL', () => {
     });
 
     expect(query5.toLocaleLowerCase()).toBe(
-      `select * from dashboards d where (d.name > "2023-10-19T00:00:00Z" and d.name > "2023-10-19T00:00:00Z") and (d.folder < "2023-10-19T00:00:00Z" and d.folder < "2023-10-19T00:00:00Z")`.toLocaleLowerCase(),
+      `where (d.name > "2023-10-19T00:00:00Z" and d.name > "2023-10-19T00:00:00Z") and (d.folder < "2023-10-19T00:00:00Z" and d.folder < "2023-10-19T00:00:00Z")`.toLocaleLowerCase(),
     );
 
     const query6 = filterToQuery({
@@ -154,7 +154,7 @@ describe('Filter to SQL', () => {
     });
 
     expect(query6.toLocaleLowerCase()).toBe(
-      `select * from dashboards d where (d.name > "2023-10-19T00:00:00Z" or d.name > "2023-10-19T00:00:00Z") and (d.folder < "2023-10-19T00:00:00Z" or d.folder < "2023-10-19T00:00:00Z")`.toLocaleLowerCase(),
+      `where (d.name > "2023-10-19T00:00:00Z" or d.name > "2023-10-19T00:00:00Z") and (d.folder < "2023-10-19T00:00:00Z" or d.folder < "2023-10-19T00:00:00Z")`.toLocaleLowerCase(),
     );
 
     const query7 = filterToQuery({
@@ -170,7 +170,7 @@ describe('Filter to SQL', () => {
     });
 
     expect(query7.toLocaleLowerCase()).toBe(
-      `select * from dashboards d where d.name > "2023-10-19T00:00:00Z"`.toLocaleLowerCase(),
+      `where d.name > "2023-10-19T00:00:00Z"`.toLocaleLowerCase(),
     );
 
     const query8 = filterToQuery({
@@ -187,7 +187,7 @@ describe('Filter to SQL', () => {
     });
 
     expect(query8.toLocaleLowerCase()).toBe(
-      `select * from dashboards d where d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z"`.toLocaleLowerCase(),
+      `where d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z"`.toLocaleLowerCase(),
     );
 
     const query9 = filterToQuery({
@@ -212,7 +212,7 @@ describe('Filter to SQL', () => {
     });
 
     expect(query9.toLocaleLowerCase()).toBe(
-      `select * from dashboards d where (d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z") OR (d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z")`.toLocaleLowerCase(),
+      `where (d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z") OR (d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z")`.toLocaleLowerCase(),
     );
 
     const query10 = filterToQuery({
@@ -253,7 +253,7 @@ describe('Filter to SQL', () => {
     });
 
     expect(query10.toLocaleLowerCase()).toBe(
-      `select * from dashboards d where ((d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z") OR (d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z")) and ((d.folder >= "2023-10-19T00:00:00Z" AND d.folder <= "2023-10-19T00:00:00Z") OR (d.folder >= "2023-10-19T00:00:00Z" AND d.folder <= "2023-10-19T00:00:00Z"))`.toLocaleLowerCase(),
+      `where ((d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z") OR (d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z")) and ((d.folder >= "2023-10-19T00:00:00Z" AND d.folder <= "2023-10-19T00:00:00Z") OR (d.folder >= "2023-10-19T00:00:00Z" AND d.folder <= "2023-10-19T00:00:00Z"))`.toLocaleLowerCase(),
     );
 
     const query11 = filterToQuery({
@@ -296,7 +296,51 @@ describe('Filter to SQL', () => {
     });
 
     expect(query11.toLocaleLowerCase()).toBe(
-      `select * from dashboards d where ((d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z") OR (d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z")) and ((d.folder >= "2023-10-19T00:00:00Z" AND d.folder <= "2023-10-19T00:00:00Z") OR (d.folder >= "2023-10-19T00:00:00Z" AND d.folder <= "2023-10-19T00:00:00Z")) OFFSET 999 LIMIT 100`.toLocaleLowerCase(),
+      `where ((d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z") OR (d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z")) and ((d.folder >= "2023-10-19T00:00:00Z" AND d.folder <= "2023-10-19T00:00:00Z") OR (d.folder >= "2023-10-19T00:00:00Z" AND d.folder <= "2023-10-19T00:00:00Z")) OFFSET 999 LIMIT 100`.toLocaleLowerCase(),
+    );
+
+    const query12 = filterToQuery({
+      from: 1000,
+      to: 1100,
+      filters: {
+        name: {
+          condition1: {
+            dateFrom: date,
+            dateTo: date,
+            filterType: 'date',
+            type: 'inRange',
+          },
+          condition2: {
+            dateFrom: date,
+            dateTo: date,
+            filterType: 'date',
+            type: 'inRange',
+          },
+          operator: 'OR',
+          filterType: 'date',
+        },
+        folder: {
+          sort: 'asc',
+          condition1: {
+            dateFrom: date,
+            dateTo: date,
+            filterType: 'date',
+            type: 'inRange',
+          },
+          condition2: {
+            dateFrom: date,
+            dateTo: date,
+            filterType: 'date',
+            type: 'inRange',
+          },
+          operator: 'OR',
+          filterType: 'date',
+        },
+      },
+    });
+
+    expect(query12.toLocaleLowerCase()).toBe(
+      `where ((d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z") OR (d.name >= "2023-10-19T00:00:00Z" AND d.name <= "2023-10-19T00:00:00Z")) and ((d.folder >= "2023-10-19T00:00:00Z" AND d.folder <= "2023-10-19T00:00:00Z") OR (d.folder >= "2023-10-19T00:00:00Z" AND d.folder <= "2023-10-19T00:00:00Z")) ORDER BY d.folder asc OFFSET 999 LIMIT 100`.toLocaleLowerCase(),
     );
   });
 });
