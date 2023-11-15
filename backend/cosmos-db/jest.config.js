023511c32928c291bb477e14c4aed72599593243
Paulo <pvpmartins@hotmail.com>
2023-11-15 14:42:24 -0300

    new jest config

--- backend/cosmos-db/jest.config.js
+++ backend/cosmos-db/jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['.*.js', '.*d.ts', '.*http.ts', '.*pv-response.ts'],
+  testPathIgnorePatterns: [
+    '.*.js',
+    '.*d.ts',
+    '.*http.ts',
+    '.*pv-response.ts',
+    '.*test-utils.ts',
+  ],
   coverage: true,
 };
