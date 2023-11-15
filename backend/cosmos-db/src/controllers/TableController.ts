1c2815547c700b24e929b2edb7c330695e56b512
Paulo <pvpmartins@hotmail.com>
2023-11-14 23:58:55 -0300

    methods handling errors

--- backend/cosmos-db/src/controllers/TableController.ts
+++ backend/cosmos-db/src/controllers/TableController.ts
@@ -1,41 +1,143 @@
-import { NextFunction } from 'express';
-import * as utils from '../utils/writer';
+import {
+  ReadPaginationFilter,
+  TableCreateReq,
+  TableDeleteReq,
+  TableReadReq,
+  TableUpdateReq,
+  TablesReadRes,
+} from 'pontus-tabler/src/pontus-api/typescript-fetch-client-generated';
+import {
+  deleteTable,
+  readTableById,
+  readTables,
+  upsertTable,
+} from '../service/TableService';
+import { Request, Response, NextFunction } from 'express';
 
-export async function tableCreatePOST(
+export const tableCreatePOST = async (
   req: Request,
   res: Response,
   next: NextFunction,
-  body,
-) {
+  body: TableCreateReq,
+): Promise<Response> => {
   try {
-    // const response = await Default.tableCreatePOST(body);
-    // utils.writeJson(res, response);
+    if (body === undefined) {
+      throw { code: 400, message: 'No properties defined' };
+    }
+    const response = await upsertTable(body);
+    res.json(response);
+    res.status(200);
+
+    return res;
   } catch (error) {
-    console.log(error);
-    utils.writeJson(res, error);
+    if (error?.code && error?.message) {
+      res.status(error.code);
+      res.json(error.message);
+      return res;
+    }
+    res.status(500);
+    res.json(error);
+    return res;
   }
-}
-export async function tableDeletePOST(req, res, next, body) {
+};
+
+export const tableReadPOST = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  body: TableReadReq,
+) => {
   try {
-    // const response = await Default.tableDeletePOST(body);
-    // utils.writeJson(res, response);
+    if (body === undefined) {
+      throw { code: 400, message: 'No properties defined' };
+    }
+    const response = await readTableById(body);
+
+    res.status(200);
+    res.json(response);
+
+    return res;
   } catch (error) {
-    utils.writeJson(res, error);
+    if (error?.code && error?.message) {
+      res.status(error.code);
+      res.json(error.message);
+      return res;
+    }
+    res.status(500);
+    res.json(error);
+    return res;
   }
-}
-export async function tableReadPOST(req, res, next, body) {
+};
+
+export const tableDeletePOST = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  body: TableDeleteReq,
+) => {
   try {
-    // const response = await Default.tableReadPOST(body);
-    // utils.writeJson(res, response);
+    const response = await deleteTable(body);
+
+    res.status(200);
+    res.json(response);
   } catch (error) {
-    next(error);
+    res.status(500);
+    res.json(error);
+    return res;
   }
-}
-export async function tableUpdatePOST(req, res, next, body) {
+};
+
+export const tableUpdatePOST = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  body: TableUpdateReq,
+) => {
   try {
-    // const response = await Default.tableUpdatePOST(body);
-    // utils.writeJson(res, response);
+    const response = await upsertTable(body);
+
+    res.status(200);
+    res.json(response);
+
+    return res;
   } catch (error) {
-    utils.writeJson(res, error);
+    res.status(500);
+    res.json(error);
+    return res;
   }
-}
+};
+
+export const tablesReadPOST = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  body: ReadPaginationFilter,
+) => {
+  try {
+    const response = await readTables(body);
+
+    const obj: TablesReadRes = {
+      tables: response.values,
+      totalTables: response.count,
+    };
+
+    res.status(200);
+    res.json(obj);
+
+    return res;
+  } catch (error) {
+    if (error?.code && error?.message) {
+      if (error.code === 404) {
+        res.status(error.code);
+        res.json([]);
+        return res;
+      }
+      res.status(error.code);
+      res.json(error.message);
+      return res;
+    }
+    res.status(500);
+    res.json(error);
+    return res;
+  }
+};
