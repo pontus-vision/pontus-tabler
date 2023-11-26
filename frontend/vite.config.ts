c2cc8fc2ee3d5491f9f399c51a42e0c267e99ebc
Paulo <pvpmartins@hotmail.com>
2023-11-26 01:56:31 -0300

    new test config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   test: {
     environment: 'happy-dom',
+    globals: true,
+    setupFiles: 'src/setupTests.js',
   },
+  plugins: [react()],
   // base: '/app_dist',
   build: {
     outDir: 'app_dist',
