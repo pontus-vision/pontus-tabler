0e5b445556c14f5be6d0cfe31fbf1714fd95b940
Paulo <pvpmartins@hotmail.com>
2023-11-29 23:02:49 -0300

    added prop, refactored code to fetch table on useEffect, and removed unecessary code

--- frontend/src/views/tables/UpdateTable.tsx
+++ frontend/src/views/tables/UpdateTable.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import NewTableCol from '../../components/NewTable/Column';
-
+import { IoChevronBackOutline } from 'react-icons/io5';
 import { getTable, getTables, updateTable } from '../../client';
 import { useTranslation } from 'react-i18next';
 import { capitalizeFirstLetter } from '../../webinyApi';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import {
   TableColumnRef,
   TableRef,
@@ -12,14 +12,18 @@ import {
 } from '../../pontus-api/typescript-fetch-client-generated';
 import TableView from '../TableView';
 
-const UpdateTableView = () => {
+type Props = {
+  tableId?: string;
+};
+
+const UpdateTableView = ({ tableId }: Props) => {
   const [cols, setCols] = useState<TableColumnRef[]>([]);
   const [table, setTable] = useState<TableRef>();
   const [tables, setTables] = useState<TableRef[]>();
   const [name, setName] = useState<string>();
 
   const params = useParams();
-
+  const navigate = useNavigate();
   const { t, i18n } = useTranslation();
 
   const fetchTable = async (id: string) => {
@@ -31,23 +35,24 @@ const UpdateTableView = () => {
   };
 
   useEffect(() => {
-    if (!params.id) return;
-    fetchTable(params.id);
-  }, [params]);
+    if (params.id || tableId) {
+      fetchTable(params.id || tableId || '');
+    }
+  }, [params, tableId]);
 
   useEffect(() => {
     setName(table?.name);
   }, [table]);
 
   const handleUpdate = async (data: TableRef) => {
-    try {
-      const updateRes = await updateTable({ ...data, id: params.id, name });
-
-      console.log({ updateRes });
-    } catch {}
+    const updateRes = await updateTable({ ...data, id: params.id, name });
   };
 
-  if (!table) return;
+  const navigateToTables = () => {
+    navigate('/tables/read');
+  };
+
+  // if (!table) return;
 
   return (
     <>
@@ -55,9 +60,14 @@ const UpdateTableView = () => {
         onChange={(e) => setName(e.target.value)}
         type="text"
         value={name}
-        className="create-table__name-input"
+        data-testid="update-table-view"
+        className="update-table__name-input"
       />
-      <TableView onUpdate={handleUpdate} table={table} />
+      <TableView onUpdate={handleUpdate} testId="update-view" table={table} />
+      <div className="update-table__tables-read-btn" onClick={navigateToTables}>
+        <IoChevronBackOutline />
+        {t('Tables')}
+      </div>
     </>
   );
 };
