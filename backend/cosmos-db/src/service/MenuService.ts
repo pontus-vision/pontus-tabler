3442236f9ae20ac526476b7a1e3fe143a8d1683a
Paulo <pvpmartins@hotmail.com>
2023-12-24 12:05:30 -0300

    using patch method in to update documents

--- backend/cosmos-db/src/service/MenuService.ts
+++ backend/cosmos-db/src/service/MenuService.ts
@@ -7,13 +7,27 @@ import {
   MenuReadRes,
 } from 'pontus-tabler/src/pontus-api/typescript-fetch-client-generated';
 import { FetchData, fetchContainer, fetchData } from '../utils/cosmos-utils';
+import { PartitionKeyDefinition, UniqueKeyPolicy } from '@azure/cosmos';
+import e = require('express');
 
 const MENU = 'menu';
+
+const partitionKey: string | PartitionKeyDefinition = {
+  paths: ['/path'],
+};
+
+const uniqueKeyPolicy: UniqueKeyPolicy = {
+  uniqueKeys: [{ paths: ['/path'] }],
+};
 
 export const upsertMenuItem = async (
   data: MenuCreateReq | MenuUpdateReq,
 ): Promise<MenuCreateRes> => {
-  const menuContainer = await fetchContainer(MENU);
+  const menuContainer = await fetchContainer(
+    MENU,
+    partitionKey,
+    uniqueKeyPolicy,
+  );
 
   for (const childIdx in data?.children) {
     const childRes = await upsertMenuItem(data.children[childIdx]);
@@ -28,9 +42,101 @@ export const upsertMenuItem = async (
   return rest;
 };
 
+// export const createMenuItem = async (
+//   data: MenuCreateReq,
+// ): Promise<MenuCreateRes | any> => {
+//   const menuContainer = await fetchContainer(
+//     MENU,
+//     partitionKey,
+//     uniqueKeyPolicy,
+//   );
+
+//   for (const childIdx in data?.children) {
+//     const childRes = await createMenuItem(data?.children[childIdx]);
+//     data.children[childIdx].id = childRes.id;
+//   }
+//   const res = await menuContainer.items.upsert(data);
+//   console.log({ res });
+
+//   const { _rid, _self, _etag, _attachments, _ts, ...rest } =
+//     res.resource as any;
+
+//   return rest;
+// };
+
+export const createMenuItem = async (
+  data: MenuCreateReq,
+): Promise<MenuCreateRes | any> => {
+  const menuContainer = await fetchContainer(
+    MENU,
+    partitionKey,
+    uniqueKeyPolicy,
+  );
+
+  const res = await menuContainer.items.create(data);
+  const { _rid, _self, _etag, _attachments, _ts, ...rest } =
+    res.resource as any;
+  return rest;
+};
+
+export const updateMenuItem = async (
+  data: MenuCreateReq | MenuUpdateReq,
+): Promise<MenuCreateRes> => {
+  const menuContainer = await fetchContainer(
+    MENU,
+    partitionKey,
+    uniqueKeyPolicy,
+  );
+
+  const patchArr = [];
+
+  for (const prop in data) {
+    switch (prop) {
+      case 'name':
+        patchArr.push({ op: 'replace', path: '/name', value: data[prop] });
+        break;
+      case 'kind':
+        patchArr.push({ op: 'replace', path: '/kind', value: data[prop] });
+        break;
+      case 'children':
+        const res = await menuContainer.items.upsert(data[prop][0]);
+        res.statusCode === 201 &&
+          patchArr.push({
+            op: 'add',
+            path: '/children/-',
+            value: res.resource,
+          });
+        break;
+      default:
+        break;
+    }
+  }
+
+  function changeLastPart(str, newPart) {
+    var n = str.lastIndexOf('/');
+    var result = str.substring(0, n + 1) + newPart;
+    return result;
+  }
+
+  // patchArr.push({
+  //   op: 'replace',
+  //   path: '/path',
+  //   value: changeLastPart(data.path, data.name),
+  // });
+
+  console.log({ id: data.id, path: data.path, patchArr });
+
+  const res = await menuContainer.item(data.id, data.path).patch(patchArr);
+
+  const { _rid, _self, _etag, _attachments, _ts, ...rest } =
+    res.resource as any;
+
+  return rest;
+};
+
 export const readMenuItemByPath = async (
   path: string,
-): Promise<MenuReadRes> => {
+): Promise<MenuReadRes | any> => {
   const querySpec = {
     query: 'select * from menu p where p.path=@path',
     parameters: [
@@ -40,20 +146,30 @@ export const readMenuItemByPath = async (
       },
     ],
   };
-  const menuContainer = await fetchContainer(MENU);
+  const menuContainer = await fetchContainer(
+    MENU,
+    partitionKey,
+    uniqueKeyPolicy,
+  );
 
   const { resources } = await menuContainer.items.query(querySpec).fetchAll();
   if (resources.length === 1) {
     return resources[0];
   } else if (resources.length === 0) {
-    throw { code: 404, message: 'No menu item found.' };
+    throw { code: 404, message: `No menu item found at path "${path}".` };
+  } else if (resources.length > 1) {
+    throw { code: 409, message: 'More than 1 record found.' };
   }
 };
 
 export const deleteMenuItem = async (data: MenuDeleteReq) => {
   try {
-    const menuContainer = await fetchContainer(MENU);
-    const res = await menuContainer.item(data.id, data.id).delete();
+    const menuContainer = await fetchContainer(
+      MENU,
+      partitionKey,
+      uniqueKeyPolicy,
+    );
+    const res = await menuContainer.item(data.id, data.path).delete();
 
     return 'menu item deleted!';
   } catch (error) {
