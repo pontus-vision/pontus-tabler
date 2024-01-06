7801f9ac20ea68b06a30c037cee31c348a9ba119
Leo <lmartins@pontusvision.com>
2024-01-06 20:56:19 +0000

    pre-merge cleanup

--- backend/azure-function-whatsapp/set-env-vars.sh
+++
@@ -1,5 +0,0 @@
-az functionapp config appsettings set --name pvhome-whatsapp --resource-group pvhomewhatsapp  --settings 'HUB_VERIFY_TOKEN=123345'
-az functionapp config appsettings set --name pvhome-whatsapp --resource-group pvhomewhatsapp  --settings 'APP_SECRET=53cc27f15eaa3a73eaec50a2fcb7e7e0'
-az functionapp config appsettings set --name pvhome-whatsapp --resource-group pvhomewhatsapp  --settings 'WHATSAPP_TOKEN=EAAMtNegzED8BO3GZAT6wb6f9dpahEAZBh4CYan83qXxH1UH3hxcVmWN84XDEYNZC6zI0ydyc1hgp5uGHdA71mYZB8pCFwgHID6cKUpW0j9EMQaBL09NAtLFg7mxMiSHIuwWThqcerxf0VmJw02FWc9Jgpw42UZClycxHxi3MxVLu7vRE4ZAJ4ZBmTfgCpNyUF2lAcOOXwb2ROiorZA3URm4rncYZD'
-az functionapp config appsettings set --name pvhome-whatsapp --resource-group pvhomewhatsapp --settings "AZURE_API_KEY=8f383b1b29274a8a93b5b243fe31e070"
-az functionapp config appsettings set --name pvhome-whatsapp --resource-group pvhomewhatsapp --settings "AZURE_SEARCH_KEY=1L4SibisLAFADAcOdj65Sqvms5vdia35fHcpANvpRAAzSeCPjtQU"
