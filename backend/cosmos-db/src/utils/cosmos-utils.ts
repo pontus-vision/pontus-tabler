4053d0bf5782a7b0e0e357bdcf65a76eb1c295f3
Paulo <pvpmartins@hotmail.com>
2024-01-12 11:59:54 -0300

    cleanup

--- backend/cosmos-db/src/utils/cosmos-utils.ts
+++ backend/cosmos-db/src/utils/cosmos-utils.ts
@@ -106,7 +106,6 @@ export const filterToQuery = (body: ReadPaginationFilter) => {
   let colSortStr = '';
 
   for (const colId in cols) {
-    console.log(colId);
     if (cols.hasOwnProperty(colId)) {
       const condition1Filter = cols[colId]?.condition1?.filter;
       const condition2Filter = cols[colId]?.condition2?.filter;
