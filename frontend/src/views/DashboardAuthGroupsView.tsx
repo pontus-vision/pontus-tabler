a935d6311467e8021b0632fb60b736b9350c4ed5
Paulo <pvpmartins@hotmail.com>
2024-03-15 12:39:15 -0300

    fetching authGroups properly

--- frontend/src/views/DashboardAuthGroupsView.tsx
+++ frontend/src/views/DashboardAuthGroupsView.tsx
@@ -6,7 +6,7 @@ import {
   ReadPaginationFilterFilters,
 } from '../typescript/api';
 import PVGridWebiny2 from '../pv-react/PVGridWebiny2';
-import { ColDef } from 'ag-grid-community';
+import { ColDef, IGetRowsParams } from 'ag-grid-community';
 import styles from './DashboardAuthGroupsView.module.scss';
 
 const DashboardAuthGroupsView = () => {
@@ -15,7 +15,9 @@ const DashboardAuthGroupsView = () => {
   const [to, setTo] = useState<number>();
   const [filters, setFilters] = useState<{
     [key: string]: ReadPaginationFilterFilters;
-  }>({});
+  }>({
+    groupName: { filter: '', filterType: 'text', type: 'contains' },
+  });
   const [cols, setCols] = useState<ColDef[]>([
     {
       headerName: 'Owner',
@@ -45,73 +47,91 @@ const DashboardAuthGroupsView = () => {
 
   const [dashboardId, setDashboardId] = useState();
 
-  const [perms, setPerms] = useState<any[]>([]);
+  const [groups, setGroups] = useState<any[]>([]);
 
   const fetchDashboards = async () => {
-    const res = await getAllDashboards({ from, to, filters });
-
+    const res = await getAllDashboards({ from: 1, to: 5, filters: {} });
+    console.log({ res });
     setDashboards(res?.data.dashboards);
   };
 
   const fetchDashboardAuthGroups = async () => {
     if (!dashboardId) return;
-    const res = await readDashboardGroupAuth({ dashboardId });
+
+    const res = await readDashboardGroupAuth({
+      dashboardId,
+      filters,
+    });
+    console.log({ res });
+
+    if (res?.status === 404) {
+      setGroups([]);
+    }
 
     const authGroups = res?.data.authGroups;
 
-    for (const prop in authGroups) {
-      authGroups[prop].forEach((el) => {
-        setPerms((prevState) =>
-          [
-            ...prevState,
-            {
-              group: el.groupName,
-              id: el.groupId,
-              create: false,
-              read: false,
-              update: false,
-              delete: false,
-              [prop]: !!el,
-            },
-          ].reduce((acc, cur) => {
-            const existingIndex = acc.findIndex(
-              (item) => item.group === cur.group && item.id === cur.id,
-            );
+    authGroups && setGroups(authGroups);
 
-            if (existingIndex !== -1) {
-              // Merge permissions by setting each to true if either is true
-              acc[existingIndex] = {
-                ...acc[existingIndex],
-                create: acc[existingIndex].create || cur.create,
-                read: acc[existingIndex].read || cur.read,
-                update: acc[existingIndex].update || cur.update,
-                delete: acc[existingIndex].delete || cur.delete,
-              };
-            } else {
-              // If no existing object, just add the curent object
-              acc.push(cur);
-            }
+    // for (const prop in authGroups) {
+    //   authGroups[prop].forEach((el) => {
+    //     setPerms((prevState) =>
+    //       [
+    //         ...prevState,
+    //         {
+    //           group: el.groupName,
+    //           id: el.groupId,
+    //           create: false,
+    //           read: false,
+    //           update: false,
+    //           delete: false,
+    //           [prop]: !!el,
+    //         },
+    //       ].reduce((acc, cur) => {
+    //         const existingIndex = acc.findIndex(
+    //           (item) => item.group === cur.group && item.id === cur.id,
+    //         );
 
-            return acc;
-          }, []),
-        );
-      });
-    }
+    //         if (existingIndex !== -1) {
+    //           // Merge permissions by setting each to true if either is true
+    //           acc[existingIndex] = {
+    //             ...acc[existingIndex],
+    //             create: acc[existingIndex].create || cur.create,
+    //             read: acc[existingIndex].read || cur.read,
+    //             update: acc[existingIndex].update || cur.update,
+    //             delete: acc[existingIndex].delete || cur.delete,
+    //           };
+    //         } else {
+    //           // If no existing object, just add the curent object
+    //           acc.push(cur);
+    //         }
+
+    //         return acc;
+    //       }, []),
+    //     );
+    //   });
+    // }
   };
 
   useEffect(() => {
     fetchDashboardAuthGroups();
-  }, [dashboardId]);
+    console.log({ dashboardId, filters, to, from });
+  }, [dashboardId, filters, to, from]);
 
   useEffect(() => {
     console.log({
-      perms: perms,
+      perms: groups,
     });
-  }, [perms]);
+  }, [groups]);
 
   useEffect(() => {
     fetchDashboards();
   }, []);
+
+  const handleParamsChange = (params: IGetRowsParams) => {
+    setFilters(params.filterModel);
+    setFrom(params.startRow + 1);
+    setTo(params.endRow);
+  };
 
   return (
     <div className={styles.dashboardAuthGroupsView}>
@@ -121,10 +141,17 @@ const DashboardAuthGroupsView = () => {
         onRowClicked={(e) => setDashboardId(e.data.id)}
       />
       <PVGridWebiny2
+        onParamsChange={handleParamsChange}
         cols={[
           {
             headerName: 'Group',
-            field: 'group',
+            field: 'groupName',
+            editable: true,
+          },
+          {
+            headerName: 'Group Id',
+            field: 'groupId',
+            hide: true,
             editable: true,
           },
           {
@@ -156,7 +183,7 @@ const DashboardAuthGroupsView = () => {
             cellRenderer: 'agCheckboxCellRenderer',
           },
         ]}
-        rows={perms}
+        rows={groups}
         totalCount={2}
       />
     </div>
