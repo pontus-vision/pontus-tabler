c696e8030707e709fd13738edb0ae0c8f4e81ae2
Paulo <pvpmartins@hotmail.com>
2024-03-20 14:52:06 -0300

    removed console.log statement

--- backend/cosmos-db/src/index.ts
+++ backend/cosmos-db/src/index.ts
@@ -96,7 +96,7 @@ export default new PontusService({
   },
   dashboardsReadPost: async (req, res) => {
     const response = await readDashboards(req.body);
-    console.log({ response });
+
     res.send(response);
   },
   dashboardUpdatePost: async (req, res) => {
