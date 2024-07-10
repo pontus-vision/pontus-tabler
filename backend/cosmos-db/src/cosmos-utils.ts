841959a8efd3a506dd2605f62d410d56d5139fa0
Paulo <pvpmartins@hotmail.com>
2024-07-10 00:14:16 -0300

    wip

--- backend/cosmos-db/src/cosmos-utils.ts
+++ backend/cosmos-db/src/cosmos-utils.ts
@@ -156,6 +156,7 @@ export const filterToQuery = (
 
       const condition1Filter = cols[col]?.condition1?.filter;
       const condition2Filter = cols[col]?.condition2?.filter;
+      const conditions = cols[col]?.conditions;
 
       const filterType = cols[col]?.filterType;
 
@@ -170,6 +171,34 @@ export const filterToQuery = (
 
       if (filterType === 'text') {
         const filter = cols[col]?.filter; // When we received a object from just one colName, the property is on a higher level
+
+        if(conditions?.length > 0) {
+          for (const condition of conditions) {
+              if (type === 'contains') {
+                colQuery.push(` CONTAINS(${alias}${colName}, "${filter}")`);
+              }
+    
+              if (type === 'not contains') {
+                colQuery.push(` NOT CONTAINS(${alias}${colName}, "${filter}")`);
+              }
+    
+              if (type === 'starts with') {
+                colQuery.push(` STARTSWITH(${alias}${colName}, "${filter}")`);
+              }
+    
+              if (type === 'ends with') {
+                colQuery.push(` ENDSWITH(${alias}${colName}, "${filter}")`);
+              }
+    
+              if (type === 'equals') {
+                colQuery.push(` ${alias}${colName} = "${filter}"`);
+              }
+    
+              if (type === 'not equals') {
+                colQuery.push(` NOT ${alias}${colName} = "${filter}"`);
+              }
+          }
+        }
 
         if (!condition1Filter) {
           if (type === 'contains') {
