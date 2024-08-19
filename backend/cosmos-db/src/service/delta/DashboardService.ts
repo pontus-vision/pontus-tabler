5acbaff917a9a67392a5e2e47344b26daa96ea70
Paulo <pvpmartins@hotmail.com>
2024-08-19 01:31:38 -0300

    methods adapted to delta

--- backend/cosmos-db/src/service/delta/DashboardService.ts
+++ backend/cosmos-db/src/service/delta/DashboardService.ts
@@ -16,6 +16,7 @@ import {
   DashboardCreateRes,
   DashboardAuthGroupsRef,
   DashboardAuthGroups,
+  Dashboard,
 } from '../../typescript/api';
 import { fetchContainer, fetchData, filterToQuery } from '../../cosmos-utils';
 import { NotFoundError } from '../../generated/api';
@@ -104,78 +105,40 @@ export const createDashboard = async (
 export const updateDashboard = async (
   data: DashboardUpdateReq,
 ): Promise<DashboardUpdateRes> => {
-  const dashboardContainer = await fetchContainer(DASHBOARDS);
+  const state = data?.state || {};
+  const sql = await updateSql(
+    DASHBOARDS,
+    { ...data, state: JSON.stringify(state) },
+    `WHERE id = '${data.id}'`,
+  );
 
-  try {
-    const patchArr: PatchOperation[] = [];
-    for (const prop in data) {
-      switch (prop) {
-        case 'name':
-          patchArr.push({
-            op: 'replace',
-            path: '/name',
-            value: data[prop],
-          });
-          break;
-        case 'state':
-          patchArr.push({
-            op: 'replace',
-            path: '/state',
-            value: data[prop],
-          });
-          break;
-        case 'folder':
-          patchArr.push({
-            op: 'replace',
-            path: '/folder',
-            value: data[prop],
-          });
-        case 'owner':
-          patchArr.push({
-            op: 'replace',
-            path: '/owner',
-            value: data[prop],
-          });
-          break;
-
-        default:
-          break;
-      }
-    }
-
-    const res3 = await dashboardContainer
-      .item(data.id, data.id)
-      .patch(patchArr);
-    return res3.resource;
-  } catch (error) {}
+  return {
+    id: sql[0]['id'],
+    name: sql[0]['name'],
+    folder: sql[0]?.['folder'],
+    state: JSON.parse(sql[0]?.['state']),
+    owner: sql[0]?.['owner'],
+  };
 };
 
 export const readDashboardById = async (dashboardId: string) => {
-  const querySpec = {
-    query: 'select * from dashboards p where p.id=@dashboardId',
-    parameters: [
-      {
-        name: '@dashboardId',
-        value: dashboardId,
-      },
-    ],
-  };
-  const dashboardContainer = await fetchContainer(DASHBOARDS);
+  const sql = await db.executeQuery(
+    `SELECT * FROM ${DASHBOARDS} WHERE id = '${dashboardId}'`,
+    conn,
+  );
 
-  const { resources } = await dashboardContainer.items
-    .query(querySpec)
-    .fetchAll();
-  if (resources.length === 1) {
-    return resources[0];
-  } else if (resources.length === 0) {
-    throw new NotFoundError('No dashboard found.');
+  if (sql.length === 0) {
+    throw new NotFoundError('Dashboard not found at id ' + dashboardId);
   }
+
+  return { ...sql[0], state: JSON.parse(sql[0]['state']) };
 };
 
 export const deleteDashboard = async (data: DashboardDeleteReq) => {
-
-  const sql = await db.executeQuery(`DELETE FROM ${DASHBOARDS} WHERE id = '${data.id}'`, conn)
-
+  const sql = await db.executeQuery(
+    `DELETE FROM ${DASHBOARDS} WHERE id = '${data.id}'`,
+    conn,
+  );
 
   const affectedRows = +sql[0]['num_affected_rows'];
 
@@ -189,20 +152,40 @@ export const deleteDashboard = async (data: DashboardDeleteReq) => {
 export const readDashboards = async (
   body: ReadPaginationFilter,
 ): Promise<DashboardsReadRes> => {
-  try {
-    const res = await fetchData(body, DASHBOARDS);
-
-    return { dashboards: res.values, totalDashboards: res.count };
-  } catch (error) {
-    if (error?.code === 404) {
-      throw new NotFoundError('No dashboards found');
-    }
+  const whereClause = filterToQuery(body);
+  const whereClause2 = filterToQuery({ filters: body.filters });
+  const sql = await db.executeQuery(
+    `SELECT * FROM ${DASHBOARDS} ${whereClause}`,
+    conn,
+  );
+  const sqlCount = await db.executeQuery(
+    `SELECT COUNT(*) FROM ${DASHBOARDS} ${whereClause2}`,
+    conn,
+  );
+  const count = +sqlCount[0]['count(1)'];
+  if (count === 0) {
+    throw new NotFoundError('No dashboards found');
   }
+
+  return {
+    dashboards: sql.map((dash) => {
+      return {
+        ...dash,
+        state: JSON.parse(dash['state']),
+      };
+    }) as Dashboard[],
+    totalDashboards: count,
+  };
 };
 
 export const createDashboardAuthGroup = async (
   data: DashboardGroupAuthCreateReq,
 ): Promise<DashboardGroupAuthCreateRes> => {
+  const sql = await db.executeQuery(
+    `SELECT name FROM ${DASHBOARDS} WHERE id = '${data.id}'`,
+    conn,
+  );
+
   const res = (await createTableDataEdge({
     edge: GROUPS_DASHBOARDS,
     edgeType: 'oneToMany',
@@ -216,7 +199,7 @@ export const createDashboardAuthGroup = async (
       rows: data.authGroups.map((group) => {
         return {
           id: data.id,
-
+          name: sql[0]['name'],
           create: group.create,
           read: group.read,
           update: group.update,
@@ -227,15 +210,36 @@ export const createDashboardAuthGroup = async (
   })) as any;
 
   return {
-    authGroups: res.map((el) => el.from) as DashboardAuthGroups[],
+    authGroups: res.map((el) => {
+      return {
+        id: el['table_from__id'],
+        name: el['table_from__name'],
+
+        create: el['table_to__create'],
+        read: el['table_to__read'],
+        update: el['table_to__update'],
+        delete: el['table_to__delete'],
+      };
+    }) as DashboardAuthGroups[],
     id: data.id,
-    name: res[0].docName,
+    name: res[0]['table_to__name'],
   };
 };
 
 export const readDashboardGroupAuth = async (
   data: DashboardGroupAuthReadReq,
 ): Promise<DashboardGroupAuthReadRes> => {
+  const filtersAdapted = {};
+
+  for (const prop in data.filters) {
+    if (prop === 'name') {
+      filtersAdapted['table_from__name'] = data.filters[prop];
+    }
+    if (prop === 'id') {
+      filtersAdapted['table_from__id'] = data.filters[prop];
+    }
+  }
+
   const res = (await readTableDataEdge({
     edge: {
       direction: 'from',
@@ -244,7 +248,7 @@ export const readDashboardGroupAuth = async (
     },
     rowId: data.id,
     tableName: DASHBOARDS,
-    filters: data.filters,
+    filters: filtersAdapted,
     from: data.from,
     to: data.to,
   })) as Record<string, any>;
@@ -288,18 +292,29 @@ export const updateDashboardGroupAuth = async (
     GROUPS_DASHBOARDS,
     data.authGroups.map((group) => {
       return {
-        ['table_from__read']: group.read,
-        ['table_from__create']: group.create,
-        ['table_from__update']: group.update,
-        ['table_from__delete']: group.delete,
-        id: group.id,
+        ['table_to__read']: group.read,
+        ['table_to__create']: group.create,
+        ['table_to__update']: group.update,
+        ['table_to__delete']: group.delete,
+        ['table_from__id']: group.id,
+        ['table_from__name']: group.id,
       };
     }),
-    `WHERE table_to__id = ${data.id}`,
+    `WHERE table_to__id = '${data.id}'`,
   );
 
   return {
-    authGroups: [],
+    authGroups: sql.map((el) => {
+      return {
+        id: el['table_from__id'],
+        name: el['table_from__name'],
+
+        create: el['table_to__create'] === 'true',
+        read: el['table_to__read'] === 'true',
+        update: el['table_to__update'] === 'true',
+        delete: el['table_to__delete'] === 'true',
+      };
+    }) as DashboardAuthGroups[],
     id: data.id,
     name: '',
   };
