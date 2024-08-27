943904278fbd11200594928523d0eb3287a4190a
Paulo <pvpmartins@hotmail.com>
2024-08-27 15:18:12 -0300

    methods refactored for delta tables (SQL)

--- backend/cosmos-db/src/service/delta/TableService.ts
+++ backend/cosmos-db/src/service/delta/TableService.ts
@@ -7,18 +7,26 @@ import {
   TableReadRes,
   TableUpdateReq,
   TablesReadRes,
+  TablesReadResTablesItem,
 } from '../../typescript/api';
-import { deleteContainer, fetchContainer, fetchData } from '../../cosmos-utils';
+import {
+  deleteContainer,
+  fetchContainer,
+  fetchData,
+  filterToQuery,
+} from '../../cosmos-utils';
 import {
   Container,
   PartitionKeyDefinition,
   UniqueKeyPolicy,
 } from '@azure/cosmos';
-import { NotFoundError } from '../../generated/api';
+import { ConflictEntityError, NotFoundError } from '../../generated/api';
 
 import * as db from '../../../../delta-table/node/index-jdbc';
-import { createSql, generateUUIDv6 } from './AuthGroupService';
+import { createSql, generateUUIDv6, updateSql } from './AuthGroupService';
+import { snakeCase } from 'lodash';
 export const TABLES = 'tables';
+export const TABLE_DATA = 'table_data';
 
 const conn: db.Connection = db.createConnection();
 const partitionKey: string | PartitionKeyDefinition = {
@@ -38,16 +46,26 @@ export const createTable = async (
 ): Promise<TableCreateRes> => {
   const uuid = generateUUIDv6();
 
+  const sqlCheck = (await db.executeQuery(
+    `SELECT * FROM ${TABLES} WHERE name = '${snakeCase(data.name)}'`,
+    conn,
+  )) as TableCreateRes[];
+
+  if (sqlCheck.length !== 0) {
+    throw new ConflictEntityError(
+      `There is already a table with this name (${data.name})`,
+    );
+  }
+
   const sql = (await db.executeQuery(
     `CREATE TABLE IF NOT EXISTS ${TABLES} (id STRING, name STRING, label STRING, cols ARRAY<STRUCT<id STRING, name STRING, field STRING, sortable BOOLEAN, header_name STRING, filter BOOLEAN, kind STRING>>) USING DELTA LOCATION '/data/pv/${TABLES}';`,
     conn,
   )) as TableCreateRes[];
 
-
-  const sql5 = (await db.executeQuery(
-    `SELECT * FROM ${TABLES};`,
-    conn,
-  )) as TableCreateRes[];
+  // const sql5 = (await db.executeQuery(
+  //   `SELECT * FROM ${TABLES};`,
+  //   conn,
+  // )) as TableCreateRes[];
 
   const cols = [];
 
@@ -55,97 +73,152 @@ export const createTable = async (
     const uuid = generateUUIDv6();
 
     cols.push(
-      `struct('${uuid}', '${col.name}', '${col.field}', ${col.sortable}, '${
-        col.headerName
-      }', ${col.filter}, ${col.kind ? `'${col.kind}` : null} )`,
+      `struct('${uuid}', '${snakeCase(col.name)}', '${col.field}', ${
+        col.sortable
+      }, '${col.headerName}', ${col.filter}, ${
+        col.kind ? `'${col.kind}'` : null
+      } )`,
     );
   }
 
-  const sql2 = await db.executeQuery(
-    `INSERT INTO ${TABLES} (id, name, label, cols) VALUES ('${uuid}', '${data.name}', '${
-      data.label
-    }', array(${cols.join(', ')}));`,
-    conn,
-  );
+  const query = `INSERT INTO ${TABLES} (id, name, label, cols) VALUES ('${uuid}', '${snakeCase(
+    data.name,
+  )}', '${data.label}', array(${cols.join(', ')}));`;
+  const sql2 = await db.executeQuery(query, conn);
 
   const sql3 = (await db.executeQuery(
     `SELECT * FROM ${TABLES} WHERE id = '${uuid}'`,
     conn,
   )) as TableCreateRes[];
 
-  return {...sql3[0], cols: JSON.parse(sql3[0].cols as any)};
+  return { ...sql3[0], cols: JSON.parse(sql3[0].cols as any) };
 };
 
 export const updateTable = async (data: TableUpdateReq) => {
-  try {
-    const tableContainer = await initiateTableContainer();
+  const cols = [];
 
-    const res = await tableContainer.items.upsert(data);
-    const { _rid, _self, _etag, _attachments, _ts, ...rest } =
-      res.resource as any;
+  // const sql2 = db.executeQuery(`SELECT * FROM ${TABLES} WHERE id = '${data.id}'`, conn)
 
-    return rest;
-  } catch (error) {
-    throw error;
+  const arr2 = [];
+
+  for (const col of data.cols) {
+    const uuid = generateUUIDv6();
+
+    const arr1 = [];
+    arr1.push(`'${col?.id || uuid}' AS id`);
+    arr1.push(`'${snakeCase(col.name)}' AS name`);
+    arr1.push(`'${col.field}' AS field`);
+    arr1.push(`${col.sortable} AS sortable`);
+    arr1.push(`'${col.headerName}' AS header_name`);
+    arr1.push(`${col.filter} AS filter`);
+    arr1.push(`'${col.kind}' AS kind`);
+    const colSnakeCase = [];
+
+    // for (const el of arr1) {
+    //   // for (const prop in col) {
+    //   //   if (typeof col[prop] === 'number' || typeof col[prop] === 'boolean') {
+    //   //     colSnakeCase.push(`${col[prop]} AS ${prop}`);
+    //   //   } else {
+    //   //     if (prop === 'name') {
+    //   //       colSnakeCase.push(`'${snakeCase(col[prop])}' AS ${prop}`);
+    //   //     } else {
+    //   //       colSnakeCase.push(`'${col[prop]}' AS ${prop}`);
+    //   //     }
+    //   //   }
+    //   // }
+    //   for (const prop in col) {
+    //     if (typeof el === 'number' || typeof el === 'boolean') {
+    //       colSnakeCase.push(el);
+    //     } else {
+    //       colSnakeCase.push(`'${el}'`);
+    //     }
+    //   }
+    //   // }
+    //   // if (!col.hasOwnProperty('id')) {
+    //   //   colSnakeCase.unshift(`'${uuid}' AS id`);
+
+    //   //   cols.push(`struct(${colSnakeCase.join(', ')})`);
+    //   // } else {
+    //   //   cols.push(`struct(${colSnakeCase.join(', ')})`);
+    //   // }
+    // }
+    cols.push(`struct(${arr1.join(', ')})`);
   }
+
+  const fields = [];
+
+  for (const prop in data) {
+    if (prop === 'cols') {
+      fields.push(`cols = array(${cols.join(', ')})`);
+      continue;
+    }
+    fields.push(
+      `${prop} = ${
+        typeof data[prop] === 'number' || typeof data[prop] === 'boolean'
+          ? data[prop]
+          : `'${data[prop]}'`
+      }`,
+    );
+  }
+
+  const strCols = `${data.name ? ` name = '${data.name}' ` : ''} ${
+    data.label ? ` label = '${data.label}' ` : ''
+  }`;
+
+  const query = `UPDATE ${TABLES} SET ${fields.join(', ')} WHERE id = '${
+    data.id
+  }';`;
+
+  const sql = await db.executeQuery(query, conn);
+
+  if (+sql[0]['num_affected_rows'] === 0) {
+    throw new NotFoundError(`did not find any record at id "${data.id}"`);
+  }
+
+  return {
+    ...data,
+  };
 };
 
 export const readTableById = async (
   data: TableReadReq,
 ): Promise<TableReadRes> => {
-  const querySpec = {
-    query: 'select * from tables p where p.id=@tableId',
-    parameters: [
-      {
-        name: '@tableId',
-        value: data.id,
-      },
-    ],
-  };
-  const tableContainer = await initiateTableContainer();
+  const sql = (await db.executeQuery(
+    `SELECT * FROM ${TABLES} WHERE id = '${data.id}'`,
+    conn,
+  )) as any;
 
-  const { resources } = await tableContainer.items.query(querySpec).fetchAll();
-
-  if (resources.length === 1) {
-    return resources[0];
-  } else if (resources.length === 0) {
-    throw new NotFoundError('Table not found');
+  if (sql.length === 1) {
+    return { ...sql[0], cols: JSON.parse(sql[0].cols) };
+  } else if (sql.length === 0) {
+    throw new NotFoundError(`No table found at id "${data.id}"`);
   }
 };
 
 export const readTableByName = async (name: string): Promise<TableReadRes> => {
-  const querySpec = {
-    query: 'select * from tables p where p.name=@tableName',
-    parameters: [
-      {
-        name: '@tableName',
-        value: name,
-      },
-    ],
-  };
+  const sql = (await db.executeQuery(
+    `SELECT * FROM ${TABLES} WHERE name = '${name}'`,
+    conn,
+  )) as any;
 
-  const tableContainer = await initiateTableContainer();
-
-  const { resources } = await tableContainer.items.query(querySpec).fetchAll();
-
-  if (resources.length === 1) {
-    return resources[0];
-  } else if (resources.length === 0) {
+  if (sql.length === 1) {
+    return { ...sql[0], cols: JSON.parse(sql[0].cols) };
+  } else if (sql.length === 0) {
     throw { code: 404, message: 'No table found.' };
   }
 };
 
 export const deleteTable = async (data: TableDeleteReq) => {
   try {
-    const tableContainer = await initiateTableContainer();
-    const res = await tableContainer.item(data.id, data.name).delete();
+    const sql = (await db.executeQuery(
+      `DELETE FROM ${TABLES} WHERE id = '${data.id}'`,
+      conn,
+    )) as any;
 
-    const res2 = (await fetchContainer(data.name)).read();
-
-    if ((await res2).statusCode === 200) {
-      const res3 = await deleteContainer(data.name);
+    const affectedRows = +sql[0]['num_affected_rows'];
+    if (affectedRows === 0) {
+      throw { code: 404 };
     }
-
     return 'Table deleted!';
   } catch (error) {
     throw error;
@@ -155,6 +228,45 @@ export const deleteTable = async (data: TableDeleteReq) => {
 export const readTables = async (
   body: ReadPaginationFilter,
 ): Promise<TablesReadRes> => {
-  const res = await fetchData(body, 'tables');
-  return { totalTables: res.count, tables: res.values };
+  const filtersSnakeCase = {};
+
+  for (const prop in body.filters) {
+    if (prop === 'name') {
+      filtersSnakeCase[prop] = {
+        ...body.filters,
+        name: {
+          ...body.filters.name,
+          filter: snakeCase(body.filters.name.filter as string),
+        },
+      };
+    }
+  }
+
+  const whereClause = filterToQuery({
+    filters: filtersSnakeCase,
+    from: body.from,
+    to: body.to,
+  });
+  const whereClause2 = filterToQuery({ filters: filtersSnakeCase });
+
+  const sql = (await db.executeQuery(
+    `SELECT * FROM ${TABLES} ${whereClause}`,
+    conn,
+  )) as TablesReadResTablesItem[];
+
+  if (sql.length === 0) {
+    throw { code: 404 };
+  }
+
+  const sql2 = await db.executeQuery(
+    `SELECT COUNT(*) FROM ${TABLES} ${whereClause2}`,
+    conn,
+  );
+
+  return {
+    totalTables: +sql2[0]['count(1)'],
+    tables: sql.map((table) => {
+      return { ...table, cols: JSON.parse(table?.cols as any) };
+    }),
+  };
 };
