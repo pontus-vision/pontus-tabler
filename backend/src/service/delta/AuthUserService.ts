f968a853e0e279db4e06031dfd4221ed1e250615
Paulo <pvpmartins@hotmail.com>
2024-09-26 13:33:21 -0300

    fixed token length check

--- backend/src/service/delta/AuthUserService.ts
+++ backend/src/service/delta/AuthUserService.ts
@@ -578,10 +578,10 @@ export const authenticateToken = async (
   await setup();
 
   const authHeader = req.headers['authorization'];
-  if (authHeader.length !== 2) {
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token.length !== 2) {
     throw { code: 400, message: 'wrong format of token' };
   }
-  const token = authHeader && authHeader.split(' ')[1];
   if (!token) {
     throw { code: 400, message: 'No token was detected in the input.' };
   }
