833b17bac73d43a11b5c5c91e98db44c3b7e7ac4
foobar <pvpmartins@hotmail.com>
2025-03-18 14:53:20 -0300

    pvhome-#32: new components

---
+++ frontend/src/views/tables/table-data/TableData.tsx
@@ -0,0 +1,306 @@
+
+import { useEffect, useRef, useState } from 'react';
+import {
+  createAuthGroup,
+  deleteAuthGroup,
+  readAuthGroups,
+  updateAuthGroups,
+} from '../../client';
+import { AuthGroupRef } from '../../typescript/api';
+import NotificationManager, {
+  MessageRefs,
+} from '../../components/NotificationManager';
+import PVGridWebiny2 from '../../pv-react/PVGridWebiny2';
+import {
+  CellClickedEvent,
+  CellValueChangedEvent,
+  IGetRowsParams,
+  IRowNode,
+} from 'ag-grid-community';
+
+type Props = {
+  onCellClicked?: (e: CellClickedEvent<any, any>) => void;
+  onRefresh?: () => void;
+  onAdd?: () => void;
+  onDelete?: (arr: any[]) => void;
+  onUpdate?: (data: any) => void;
+  permissions?: {
+    updateAction?: boolean;
+    createAction?: boolean;
+    deleteAction?: boolean;
+    readAction?: boolean;
+  };
+  groupsToFilterOutById?: AuthGroupRef[];
+  onRowsSelected?: (e: IRowNode<any>[]) => void;
+  selection?: boolean;
+  selectRowByCell?: boolean;
+  onParamsChange?: (params: IGetRowsParams) => void;
+  isLoading?: boolean;
+  onRowsStateChange?: (data: Record<string, any>[]) => void;
+  onCellsChange?: (data: CellValueChangedEvent[]) => void;
+};
+
+const AuthGroups = ({
+  isLoading,
+  onAdd,
+  onCellClicked,
+  onCellsChange,
+  onDelete,
+  onParamsChange,
+  onRefresh,
+  onRowsStateChange,
+  onUpdate,
+  selection,
+  permissions,
+  selectRowByCell,
+  onRowsSelected,
+  groupsToFilterOutById,
+}: Props) => {
+  const [totalGroups, setTotalGroups] = useState<number>();
+  const [isLoading1, setIsLoading1] = useState(false);
+  const [authGroups, setAuthGroups] = useState<AuthGroupRef[]>([]);
+  const [selectedGroup, setSelectedGroup] = useState<AuthGroupRef | null>();
+  const notificationManagerRef = useRef<MessageRefs>();
+  const [addMode, setAddMode] = useState(false);
+  const [updateMode, setUpdateMode] = useState(false);
+  const [groupsChanged, setGroupsChanged] = useState<AuthGroupRef[]>([]);
+  const [groupsToBeUpdated, setGroupsToBeUpdated] = useState<AuthGroupRef[]>(
+    [],
+  );
+  const [groupsToBeAdded, setGroupsToBeAdded] = useState<AuthGroupRef | null>();
+
+  const [reset, setReset] = useState(false);
+
+  const fetchAuthGroups = async () => {
+    setIsLoading1(true);
+    const res = await readAuthGroups({ from: 1, to: 100, filters: {} });
+
+    const authGroups = res?.data.authGroups;
+
+    if (res?.status === 404) {
+      setAuthGroups([]);
+      setTotalGroups(0);
+      setIsLoading1(false);
+      return;
+    } else if (res?.status === 500) {
+      notificationManagerRef?.current?.addMessage(
+        'error',
+        'Error',
+        'Something went wrong. Could not fetch Auth Group(s)!',
+      );
+      setAuthGroups([]);
+      setTotalGroups(0);
+      setIsLoading1(false);
+      return;
+    }
+
+    if (groupsToFilterOutById) {
+      const filtered = res?.data.authGroups?.filter(
+        (dash1) =>
+          !groupsToFilterOutById.some((dash2) => dash1.id === dash2.id),
+      );
+
+      filtered && setAuthGroups(filtered);
+      setTotalGroups(filtered?.length);
+    } else {
+      authGroups && setAuthGroups(authGroups);
+      setTotalGroups(res?.data.totalGroups);
+    }
+    setIsLoading1(false);
+  };
+
+  const delAuthGroup = async (arr: AuthGroupRef[]) => {
+    for (const [index, group] of arr.entries()) {
+      const res = await deleteAuthGroup({ id: group.id });
+
+      if (index === arr.length - 1) {
+        if (res?.status === 200) {
+          notificationManagerRef?.current?.addMessage(
+            'success',
+            'Success',
+            'Auth Group(s) deleted!',
+          );
+          await fetchAuthGroups();
+        } else {
+          notificationManagerRef?.current?.addMessage(
+            'error',
+            'Error',
+            'Something wrong happened! Could not delete.',
+          );
+        }
+      }
+    }
+  };
+
+  const addGroup = async (data: AuthGroupRef) => {
+    if (!addMode || data.id) return;
+
+    const res = await createAuthGroup({ name: data.name });
+
+    if (res?.status === 200) {
+      notificationManagerRef?.current?.addMessage(
+        'success',
+        'Success',
+        `AuthGroup(s) created!`,
+      );
+
+      await fetchAuthGroups();
+    } else {
+      notificationManagerRef?.current?.addMessage(
+        'error',
+        'Error',
+        'Something went wrong. Could not create Auth Group(s)!',
+      );
+    }
+
+    setGroupsToBeAdded(null);
+    // setGroupsChanged(groupsChanged.filter((group) => !!group.id));
+    // console.log({ groupsChanged });
+    resetRowsState();
+    setGroupsChanged([]);
+    setAddMode(false);
+  };
+
+  const resetRowsState = () => {
+    setReset(true);
+    setTimeout(() => {
+      setReset(false);
+    }, 1);
+  };
+
+  const updateGroups = async () => {
+    console.log({ groupsChanged });
+    if (groupsChanged.length === 0) return;
+    const fails = [];
+
+    for (const [index, group] of groupsChanged.entries()) {
+      if (!group.id) break;
+      const res = await updateAuthGroups({ id: group.id, name: group.name });
+
+      if (res.status !== 200) {
+        notificationManagerRef?.current?.addMessage(
+          'error',
+          'Error',
+          `Something went wrong. Could not update Auth Group to ${group.name}.`,
+        );
+        fails.push(res.status);
+      }
+
+      if (index === groupsToBeUpdated.length - 1 && fails.length === 0) {
+        notificationManagerRef?.current?.addMessage(
+          'success',
+          'Success',
+          `Auth Group(s) updated.`,
+        );
+      }
+    }
+    resetRowsState();
+    setGroupsChanged([]);
+    setGroupsChanged([]);
+    setGroupsToBeUpdated([]);
+
+    setUpdateMode(false);
+  };
+
+  const handleOnCellsChange = (e: CellValueChangedEvent<any, any>[]) => {
+    if (onCellsChange) {
+      return onCellsChange;
+    }
+  };
+
+  const handleOnAdd = () => {
+    if (onAdd) {
+      return onAdd;
+    }
+
+    setAddMode(true);
+    setTotalGroups((totalGroups || 0) + 1);
+    setAuthGroups([...authGroups, { name: '', id: '' }]);
+  };
+
+  const handleRowsStateChange = (e: Record<string, any>[]) => {
+    // if(authGroups.some(group=> ))
+    setGroupsChanged(e as AuthGroupRef[]);
+
+    // if (e.some((el) => !el.id)) {
+    //   setAddMode(true);
+    // } else {
+    //   setUpdateMode(true);
+    // }
+
+    // if (addMode) {
+    //   setGroupsToBeAdded(e[e.length - 1] as AuthGroupRef);
+    // }
+  };
+
+  useEffect(() => {
+    if (!groupsChanged[groupsChanged.length - 1]?.id) {
+      setAddMode(true);
+      setUpdateMode(false);
+    } else {
+      setAddMode(false);
+      setUpdateMode(true);
+    }
+    if (addMode) {
+      setGroupsToBeAdded(
+        groupsChanged[groupsChanged.length - 1] as AuthGroupRef,
+      );
+    }
+    setGroupsToBeUpdated(groupsChanged.filter((group) => group.id));
+  }, [groupsChanged]);
+
+  useEffect(() => {
+    fetchAuthGroups();
+  }, []);
+
+  useEffect(() => {
+    // groupsToBeAdded && addGroup(groupsToBeAdded);
+  }, [groupsToBeAdded]);
+
+  return (
+    <>
+      <PVGridWebiny2
+        onCellClicked={onCellClicked}
+        cypressAtt="auth-groups-grid"
+        onRefresh={() => fetchAuthGroups()}
+        add={() => handleOnAdd()}
+        onDelete={(e) => delAuthGroup(e)}
+        permissions={permissions}
+        selection={selection}
+        onParamsChange={onParamsChange}
+        isLoading={isLoading1}
+        onRowsSelected={onRowsSelected}
+        updateModeOnRows={true}
+        onUpdate={updateGroups}
+        resetRowsChangedState={reset}
+        onCellValueChange={(e) => addGroup(e.data)}
+        onCellsChange={(e) => handleOnCellsChange(e)}
+        selectRowByCell={selectRowByCell}
+        onRowsStateChange={(e) => handleRowsStateChange(e)}
+        cols={[
+          {
+            headerName: 'Name',
+            field: 'name',
+            sortable: true,
+
+            filter: true,
+            editable: permissions?.updateAction,
+            // cellEditor: SimpleTextEditor,
+          },
+          {
+            headerName: 'Id',
+            field: 'id',
+            sortable: true,
+            filter: true,
+          },
+        ]}
+        rows={authGroups}
+        totalCount={totalGroups}
+      />
+
+      <NotificationManager ref={notificationManagerRef} />
+    </>
+  );
+};
+
+export default AuthGroups;
---
+++ frontend/src/views/users/RegisterUser.tsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../AuthContext';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import useApiAndNavigate from '../../hooks/useApi';
+import useLogin from '../../hooks/useLogin';
+import { registerUser } from '../../client';
+
+const RegisterUser = () => {
+  const { login, isAuthenticated } = useAuth();
+  const [role, setRole] = useState('Admin');
+  const [apiKeys, setApiKeys] = useState();
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [username, setUsername] = useState('');
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { fetchDataAndNavigate } = useApiAndNavigate();
+
+  const { handleLogin, loading } = useLogin();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      console.log({ isAuthenticated })
+      navigate('/tables/read')
+    }
+  }, [])
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+    if (password2 !== password) return
+    const res = await registerUser({ password, passwordConfirmation: password2, username })
+
+    console.log({ res })
+
+    handleLogin(username, password);
+  }
+
+  if (isAuthenticated) return
+
+  return (
+    <div className="login-page">
+      <div className="login-form">
+        <section className="welcome-msg">
+          <img src="/src/assets/pontus-logo.png" alt="" />
+          <h1>Welcome to the Pontus Vision Platform!</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
+            ratione eius vel molestiae sit autem ipsa facere vitae quod commodi?
+          </p>
+        </section>
+        <section className="form">
+          <form onSubmit={loginUser}>
+            <h3>Register</h3>
+            <div className="mb-3">
+              <label>Username</label>
+              <input
+                data-cy="username-login-input"
+                onChange={(e) => setUsername(e.target.value)}
+                type="text"
+                className="form-control"
+                placeholder="Enter username"
+              />
+            </div>
+            <div className="mb-3">
+              <label>Password</label>
+              <input
+                data-cy="register-user-password-input"
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="mb-3">
+              <label>Password Confirmation</label>
+              <input
+                data-cy="register-user-password-confirmation-input"
+                onChange={(e) => setPassword2(e.target.value)}
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="mb-3">
+              <div className="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customCheck1"
+                />
+                <label className="custom-control-label" htmlFor="customCheck1">
+                  Remember me
+                </label>
+              </div>
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            <p className="forgot-password text-right">
+              Forgot <a href="#">password?</a>
+            </p>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterUser;
