474a4fdbad31101d79f9902acecbcee5b96f07aa
Paulo <pvpmartins@hotmail.com>
2025-04-09 23:13:19 -0300

    pvhome-#102-c: wip

--- backend/src/__tests__/AuthGroups.test.ts
+++ backend/src/__tests__/AuthGroups.test.ts
@@ -46,7 +46,6 @@ import { prepareDbAndAuth } from './test-utils';
 import { AxiosResponse } from 'axios';
 import { AUTH_GROUPS, AUTH_USERS, DASHBOARDS, TABLES, DELTA_DB, GROUPS_DASHBOARDS, GROUPS_USERS } from '../consts';
 import { runQuery } from '../db-utils';
-import { exit } from 'process';
 
 // // Mock the utils.writeJson function
 // jest.mock('../utils/writer', () => ({
@@ -83,7 +82,6 @@ describe('dashboardCreatePOST', () => {
     }
     process.env = OLD_ENV; // Restore old environment
     srv.close();
-    exit()
   });
 
   it('should create a group', async () => {
