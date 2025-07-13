ee7d785f491f5c58ca8a8186953a6e59291550a7
PV <pvpmartins@hotmail.com>
2025-07-13 12:27:52 -0300

    pvhome-#99: changed name property

---
+++ backend/database-management/migrations/008-create-audit.sql
@@ -0,0 +1,12 @@
+-- changeset you:008-create-auditions
+-- description: create-audit
+
+CREATE TABLE IF NOT EXISTS $SCHEMA_NAME.audit (
+    session_id STRING,
+    api_path STRING,
+    body STRING,
+    error STRING,
+    user_id STRING,
+    group_ids STRING,
+    created_at TIMESTAMP
+) USING DELTA LOCATION "/data/$SCHEMA_NAME/audit";
