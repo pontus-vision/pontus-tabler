c3e6ffb81d4cf6319dcecb6bd0f69f3c8fb1d38e
Paulo Martins <pvpmartins@hotmail.com>
2025-09-17 13:18:33 -0300

    source changed to job_scheduler folder

--- backend/delta-table/work-dir/test/run-tests.sh
+++ backend/delta-table/work-dir/test/run-tests.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+DIR="$(
+  cd "$(dirname "$0")"
+  pwd -P
+)"
 cd "$DIR"/
 
-coverage run --source=../udl/ -m behave -e 'features/.ipynb_checkpoints/*' $* 
+coverage run --source=../job_scheduler/ -m behave -e 'features/.ipynb_checkpoints/*' $*
 #coverage run --source=../udl/ -m behave --no-capture
 coverage report -m
